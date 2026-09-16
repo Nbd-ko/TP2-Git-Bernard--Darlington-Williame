ef9637c16bbd4f9d0343cb72a41f3588164f32ae
Léo Williame <williaml@3il.fr>
2026-09-16 16:46:19 +0200

    Ajout d'une deuxième ligne au message B

--- src/MessageB.java
+++ src/MessageB.java
@@ -1,5 +1,6 @@
 public class MessageB { 
     public static void main(String[] args) { 
         System.out.println("Yo les p'tits potes !"); 
+        System.out.println("Je vais bien et toi poto ?")
     } 
 }
